c9a1d9ac069725ed4e468dfd1b9f5d9f596dbae5
shubham.shingate <shubham.shingate@VivekM.dgsl.biz>
2019-10-15 16:00:53 +0530

    Summary: 1st Commit of Project Creation(ShubhamS)

---
+++ src/main/java/com/datamatics/dccV1/DccV1Application.java
@@ -0,0 +1,13 @@
+package com.datamatics.dccV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DccV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DccV1Application.class, args);
+	}
+
+}
